26a7334c55cf987d7463b252c06fc490e7cc0ebe
Gary Rusher <gary.rusher@vizientinc.com>
2019-01-11 13:34:21 -0600

    first checkin

---
+++ website_example/config.js
@@ -0,0 +1,14 @@
+var api = "https://imaginary.stream/api";
+var poolHost = "graft.imaginary.stream";
+
+var email = "graft@imaginary.stream";
+var telegram = "https://t.me/ImaginaryStream";
+var discord = "";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "https://graft.observer/block/{id}";
+var transactionExplorer = "https://graft.observer/tx/{id}";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
